99603d87894c3246f5821f4b28bf814ab9241718
A.ARMIJOS <aarmijosc@a.mikeldi.com>
2026-03-17 11:41:14 +0100

    Commit inicial

---
+++ killerfy/src/main/java/com/killerfy/KillerfyApplication.java
@@ -0,0 +1,13 @@
+package com.killerfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KillerfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KillerfyApplication.class, args);
+	}
+
+}
---
+++ killerfy/src/test/java/com/killerfy/KillerfyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.killerfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KillerfyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
